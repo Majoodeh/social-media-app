5c5da255037334ed36332f8834ed72864797e356
Majoodeh <Majdhussein144@gmail.com>
2026-03-27 22:59:31 +0100

    feat: initial commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,9 @@
+// this file is used to wrap the auth pages and provide a common layout for them. It can be used to add a background image, a logo, or any other common elements that should be present on all auth pages.
+
+import React from "react";
+
+const AuthLayout = () => {
+  return <div>AuthLayout</div>;
+};
+
+export default AuthLayout;
---
+++ src/_root/pages/index.ts
@@ -0,0 +1,10 @@
+export { default as Home } from "./Home";
+
+// Why is that file for?
+// THis file is used to export all the pages in the application. This way, we can import them from a single file instead of importing them from their respective files.
+// so instead of:
+// import Home from "./Home";
+//  import Profile from "./Profile";
+// we can do:
+// import { Home, Profile } from "./pages";
+// so it is just a convenience file to export all the pages in the application. It does not contain any logic or code, it just exports the pages.
